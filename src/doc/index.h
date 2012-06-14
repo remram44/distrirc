052f096fe07f6177a69c6dc74f17082658969b67
remram44 <remirampin@gmail.com>
2012-06-13 20:14:16 -0400

    Minor fix to the doc's frontpage.

--- src/doc/index.h
+++ src/doc/index.h
@@ -7,9 +7,9 @@
  * The main motivation is to use two programs on two different machines: a
  * relay on some shell that is always connected to the Internet, and a client
  * on a desktop machine that will appear to stay online thanks to the relay,
- * and will be able to access the logs seemlessly; in that aspect it is very
- * similar to @link http://quassel-irc.org/ Quassel @endlink, but it can also
- * be used in more complicated setups. It also tries to conform to the IRC
+ * and will be able to access the logs seemlessly. In that aspect, it is very
+ * similar to <a href="http://quassel-irc.org/">Quassel</a>, but it can also be
+ * used in more complicated setups. It also tries to conform to the IRC
  * protocol (for the relayed connections).
  *
  * See the @link protocol protocol overview @endlink for more information.
